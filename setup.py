4a830ae58504761d96374a13c06d051f6d3312a8
pavelsof <pavelsof@gmail.com>
2017-05-25 16:24:02 +0200

    Uploaded to PyPI.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import os.path
 
 from setuptools import setup, find_packages
 
+from ipalint import __version__
+
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -14,14 +16,14 @@ with open(os.path.join(BASE_DIR, 'README.rst')) as f:
 
 setup(
 	name = 'ipalint',
-	version = '0.0.0',
+	version = __version__,
 	
 	description = (
 		'simple linter that checks datasets '
 		'for IPA errors and inconsistencies'),
 	long_description = README,
 	
-	# url = '',
+	url = 'https://github.com/pavelsof/ipalint',
 	
 	author = 'Pavel Sofroniev',
 	author_email = 'pavelsof@gmail.com',
@@ -29,7 +31,7 @@ setup(
 	license = 'MIT',
 	
 	classifiers = [
-		'Development Status :: 3 - Alpha',
+		'Development Status :: 4 - Beta',
 		'Environment :: Console',
 		'Intended Audience :: Science/Research',
 		'License :: OSI Approved :: MIT License',
